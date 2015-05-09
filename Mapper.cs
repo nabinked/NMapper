4b1f3a40a9a7981ee4f8fd82e147fb391274f6da
Nabin Karki Thapa <nabin@outlook.com>
2015-05-09 21:25:51 +0545

    Removing dependency on custom attributes. Performance improvement

--- Mapper.cs
+++ Mapper.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Reflection;
+using nMapper;
 using NMapperAttributes;
 
 namespace NMapper
 {
     public class Mapper<T> where T : new()
     {
-        private List<string> _columnNames;
+        private List<string> ColumnNames { get; set; }
 
         private Dictionary<string, PropertyInfo> AttributeToPropInfoMapping { get; set; }
+
+        private List<PropertyInfo> OrderedPropertyInfos { get; set; }
 
         public Mapper()
         {
@@ -21,36 +24,70 @@ namespace NMapper
         private void Construct()
         {
             Type = typeof(T);
-            var tableInfo = (TableInfoAttribute)Attribute.GetCustomAttribute(Type, typeof(TableInfoAttribute));
-            if (tableInfo != null)
-            {
-                Schema = tableInfo.SchemaName;
-                TableName = tableInfo.TableName;
-            }
-            PropertyInfo = Type.GetProperties();
+            //var tableInfo = (TableInfoAttribute)Attribute.GetCustomAttribute(Type, typeof(TableInfoAttribute));
+            //if (tableInfo != null)
+            //{
+            //    Schema = tableInfo.SchemaName;
+            //    TableName = tableInfo.TableName;
+            //}
+
+            PropertyInfos = new List<PropertyInfo>(Type.GetProperties());
             SetAttributePropertyMapping();
 
         }
 
+        private void OrderPropertyInfos()
+        {
+            OrderedPropertyInfos = new List<PropertyInfo>();
+            foreach (var name in ColumnNames)
+            {
+                var info = GetPropertyInfoForOrderedList(name);
+                if (info != null)
+                {
+                    OrderedPropertyInfos.Add(info);
+                }
+
+            }
+        }
+
+        private PropertyInfo GetPropertyInfoForOrderedList(string colunmName)
+        {
+            if (IsColumnNameAttributeDefined)
+            {
+                PropertyInfo info;
+                AttributeToPropInfoMapping.TryGetValue(colunmName, out info);
+                return info;
+            }
+            else
+            {
+                return PropertyInfos.FirstOrDefault(info => info.Name == NamingConvention.ProcessColumnNamesToObjectNames(colunmName));
+            }
+        }
+
         private void SetAttributePropertyMapping()
         {
-            var props = Type.GetProperties().Where(prop => Attribute.IsDefined(prop, typeof(ColumnNameAttribute)));
+            var propertyInfos = Type.GetProperties().Where(prop => Attribute.IsDefined(prop, typeof(ColumnNameAttribute))).ToList();
+            if (!propertyInfos.Any()) return;
+
+            IsColumnNameAttributeDefined = true;
             AttributeToPropInfoMapping = new Dictionary<string, PropertyInfo>();
-            foreach (var propertyInfo in props)
+            foreach (var propertyInfo in propertyInfos)
             {
                 var colName = propertyInfo.GetCustomAttribute(typeof(ColumnNameAttribute), true) as ColumnNameAttribute;
                 if (colName != null) AttributeToPropInfoMapping.Add(colName.ColumnName, propertyInfo);
             }
         }
 
-        private PropertyInfo[] PropertyInfo { get; set; }
+        private List<PropertyInfo> PropertyInfos { get; set; }
+
+        public bool IsColumnNameAttributeDefined { get; set; }
 
         private string Schema { get; set; }
 
         private string TableName { get; set; }
 
         private Type Type { get; set; }
-        
+
         /// <summary>
         /// Gets the T Object from the command
         /// </summary>
@@ -63,7 +100,7 @@ namespace NMapper
             var t = new T();
             while (rdr.Read())
             {
-                _columnNames = Enumerable.Range(0, rdr.FieldCount).Select(rdr.GetName).ToList();
+                ColumnNames = Enumerable.Range(0, rdr.FieldCount).Select(rdr.GetName).ToList();
                 SetProperties(rdr, t);
                 command.Connection.Close();
                 break;
@@ -84,7 +121,13 @@ namespace NMapper
             while (rdr.Read())
             {
                 var t = new T();
-                _columnNames = Enumerable.Range(0, rdr.FieldCount).Select(rdr.GetName).ToList();
+                if (!IsNotFirstRow)
+                {
+                    ColumnNames = Enumerable.Range(0, rdr.FieldCount).Select(rdr.GetName).ToList();
+                    OrderPropertyInfos();
+                    IsNotFirstRow = true;
+                }
+                if (ColumnNames.Count != OrderedPropertyInfos.Count) throw new Exception("Not all columns are mapped to object properties. Please Make sure that all the columns have thier respective properties or that they follow the specified naming conventions or that their names are not misspelled.");
                 SetProperties(rdr, t);
                 tList.Add(t);
             }
@@ -92,19 +135,23 @@ namespace NMapper
             return tList;
         }
 
+        private bool IsNotFirstRow { get; set; }
+
+
         private void SetProperties(IDataRecord rdrRow, T t)
         {
             if (rdrRow == null) throw new ArgumentNullException("rdr");
-            foreach (var columnName in _columnNames)
+            for (var i = 0; i < ColumnNames.Count; i++)
             {
-                PropertyInfo property;
-                if (!AttributeToPropInfoMapping.TryGetValue(columnName, out property)) continue;
-                var value = rdrRow[columnName];
-                var propType = Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType;
+                var propertyInfo = OrderedPropertyInfos[i];
+                var value = rdrRow[i];
+                var propType = Nullable.GetUnderlyingType(propertyInfo.PropertyType) ?? propertyInfo.PropertyType;
                 var safeValue = (value == null || value == DBNull.Value) ? null : Convert.ChangeType(value, propType);
-                property.SetValue(t, safeValue, null);
+                propertyInfo.SetValue(t, safeValue, null);
             }
         }
+
+
 
     }
 }
